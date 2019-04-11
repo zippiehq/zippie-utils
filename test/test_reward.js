a600c5ce4fff47a4ce4f4f8fe137c5ef19a7c4e5
Lewis Rockliffe <r0kk3rz@gmail.com>
2019-04-11 10:31:02 +1000

    add reward-api and tests

--- test/test_reward.js
+++ test/test_reward.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect
 const reward = require('../src/reward')
+const utils = require('../src/utility')
 const testTokens = require('./test_tokens')
 
 describe('test: reward', function () {
@@ -8,7 +9,7 @@ describe('test: reward', function () {
     Object.keys(testTokens).forEach(key => {
         it('getRewardTokens ' + key, function(done) {
         let token = testTokens[key]
-        reward.getRewardTokens(token.amount, token.message, token.address)
+        reward.getRewardTokens(token.address, token.amount, token.message, token.address)
             .then(result => {
                 expect(result).to.contain(token.appUri)
                 done()
@@ -17,5 +18,56 @@ describe('test: reward', function () {
                 done(error)
             })
         })      
+    })
+
+    const userId = utils.randomKey(64).toString('hex')
+    console.log('UserID', userId)
+
+    Object.keys(testTokens).forEach(key => {
+        it('rewardTo ' + key, function(done) {
+            let token = testTokens[key]
+            reward.rewardTo(userId, token.address, token.amount, 'apikey').then(result => {
+                expect(result).to.contain({success: true})
+                done()
+            }).catch(error=> {
+                done(error)
+            })
+        })
+    })
+
+    Object.keys(testTokens).forEach(key => {
+        it('getUserBalance ' + key, function(done) {
+            let token = testTokens[key]
+            reward.getUserBalance(userId, token.address, 'apikey').then(result => {
+                expect(result).to.contain.all.keys(['balance', 'pending', 'wallets', 'cheques'])
+                done()
+            }).catch(error=> {
+                done(error)
+            })
+        })
+    })
+
+    Object.keys(testTokens).forEach(key => {
+        it('createPendingCheque ' + key, function(done) {
+            let token = testTokens[key]
+            reward.createPendingCheque(userId, token.address, 'apikey').then(result => {
+                console.log(result)
+                done()
+            }).catch(error=> {
+                done(error)
+            })
+        })
+    })
+
+    Object.keys(testTokens).forEach(key => {
+        it('getCheques ' + key, function(done) {
+            let token = testTokens[key]
+            reward.getCheques(userId, token.address, 'apikey').then(result => {
+                console.log(result)
+                done()
+            }).catch(error=> {
+                done(error)
+            })
+        })
     })
 })
